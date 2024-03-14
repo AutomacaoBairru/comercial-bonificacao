3807f152e8ed51b8dd90c1070f7c47b7cebea983
Julio Marques <jmarques@bairru.com.br>
2024-03-14 11:12:25 -0300

    Ajuste na exibição da data e id propostas do vimob

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -3,7 +3,8 @@ import axios, { AxiosInstance } from "axios";
 
 const instance: AxiosInstance = axios.create({
   baseURL: "https://api-servicos-automacao.bairru.com.br/",
-  timeout: 30000, // Definir o timeout como 30 segundos
+  // baseURL: "http://127.0.0.1:5000/",
+  timeout: 30000, 
   
 });
 
